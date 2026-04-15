586f3dd389b88244072e815fb434c1e38f53448d
akibana <akibana@C010422871.zipangu.cac.co.jp>
2026-04-15 16:39:38 +0900

    challenge08 コミットプッシュ

--- Java100_questions_cac2nd/src/lesson03/challenge08/Explorer.java
+++ Java100_questions_cac2nd/src/lesson03/challenge08/Explorer.java
@@ -12,7 +12,7 @@
  * 6時間以内に洞窟に入らなければ全滅してしまいます。
  *
  * コメントの位置にdo～while文とif文を利用した処理を記述して
- * 実行例と同じメッセージを表示してください。
+// * 実行例と同じメッセージを表示してください。
  *
  * <実行例>
  *  隊長：
@@ -58,16 +58,30 @@ public class Explorer {
 
 		int lithograph = 0;
 		int i = 0;
-
+		int Flag = 0;
+		//int lithograph = (int) Math.random() * 10 + 1;
 
 		//ここにdo～while文、if文を利用した処理を記述
 
+		do {
+			if (Flag == 1) {
+				break;
+			}
+			i += 1;
+			lithograph = (int) (Math.random() * 10) + 1;
 
-		if (lithograph == 3) {
-			System.out.println("洞窟の入り口が開きました。");
-		} else {
-			System.out.println("隊長：");
-			System.out.println(lithograph + "だった...（" + (i - 1)+ "時間経過）");
+			if (lithograph == 3) {
+				System.out.println("やったー！3が出たよ！");
+				System.out.println("洞窟の入り口が開きました。");
+				Flag = 1;
+			} else {
+				System.out.println("隊長：");
+				System.out.println(lithograph + "だった...（" + (i - 1) + "時間経過）");
+				System.out.println("1時間待つよ");
+			}
+		} while (i <= 6);
+
+		if (Flag == 0) {
 			System.out.println("あの時サバ缶に手を付けていなければ...");
 			System.out.println("探検隊は全滅しました。");
 		}
