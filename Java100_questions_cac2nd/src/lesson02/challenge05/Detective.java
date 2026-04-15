f976acb2453c28c1a6355c0ca291f4bc6e93ff2d
akibana <akibana@C010422871.zipangu.cac.co.jp>
2026-04-15 09:57:01 +0900

    lesson02 challenge01~05 コミットプッシュ

--- Java100_questions_cac2nd/src/lesson02/challenge05/Detective.java
+++ Java100_questions_cac2nd/src/lesson02/challenge05/Detective.java
@@ -38,7 +38,7 @@ public class Detective {
 
 	public static void main(String[] args) {
 
-		int height = 190;
+		int height = 186;
 
 		System.out.println("探偵：");
 		System.out.println("宝石を置いていた場所はどこですか？\n");
@@ -48,6 +48,12 @@ public class Detective {
 		System.out.println("犯人は...");
 
 		// ここにif～else文を入れる
+
+		if (height > 185) {
+			System.out.println("\n背の高い人ですね");
+		} else {
+			System.out.println("\n背の低い人ですね");
+		}
 
 	}
 
